f737e8c99063850a5e27f22b9216eb5707302e8f
icetree158 <danya.strelczov.2001@gmail.com>
2023-03-23 04:45:09 +0300

    end

--- caret-position.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "caret-position" {
-    function get(element: HTMLElement): number | undefined;
-    function set(element: HTMLElement, position: number): void;
-    function getRect(element: HTMLElement, position?: number): ClientRect | undefined;
-  }
